ae0baf59d916386f30fac570195da533749c421a
Anborgh <divoannet@gmail.com>
2020-11-07 14:12:44 +0300

    run server over https

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-var express = require('express'),
-    app = express(),
-    port = process.env.PORT || 3000,
-    mongoose = require('mongoose'),
-    Player = require('./api/models/playerModel'),
-    Flags = require('./api/models/flagModel'),
-    bodyParser = require('body-parser');
+var fs = require('fs');
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+var mongoose = require('mongoose');
+var Player = require('./api/models/playerModel');
+var Flags = require('./api/models/flagModel');
+var bodyParser = require('body-parser');
+
+var key = fs.readFileSync(process.env.KEY_PATH);
+var cert = fs.readFileSync(process.env.CERT_PATH);
+var ca = fs.readFileSync(process.env.CA_PATH);
+
+var options = {
+  key: key,
+  cert: cert,
+  ca: ca
+};
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
@@ -18,6 +29,5 @@ app.use(bodyParser.json());
 var routes = require('./api/routes/allNewGameRoutes'); //importing route
 routes(app);
 
-app.listen(port);
-
-console.log('todo list RESTful API server started on: ' + port);
+var https = require('https');
+https.createServer(options, app).listen(3000);
